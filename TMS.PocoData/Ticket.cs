de9938812437139ca4d4588e719923487c561f79
Yubing325 <35515298+Yubing325@users.noreply.github.com>
2019-11-05 15:11:36 -0600

    Add Ticket POCO

---
+++ TMS.PocoData/Ticket.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.PocoData
+{
+    public class Ticket
+    {
+        public int TicketId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime CreatedTime { get; set; }
+
+        public DateTime UpdateTime { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+    }
+}
