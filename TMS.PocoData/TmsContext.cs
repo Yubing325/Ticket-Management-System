28f4ab59c36daf2cf604b9e2cf28595d7c0e1803
Yubing325 <35515298+Yubing325@users.noreply.github.com>
2019-11-05 15:39:32 -0600

    Add dbcontext and connection string

---
+++ TMS.PocoData/TmsContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.PocoData
+{
+    public class TmsContext:DbContext
+    {
+        public TmsContext(DbContextOptions<TmsContext> options)
+                :base(options)
+        {
+
+        }
+
+        public DbSet<Ticket> Tickets { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Role> Roles { get; set; }
+    }
+}
--- TMS.TicketManagementSystem/Startup.cs
+++ TMS.TicketManagementSystem/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using TMS.PocoData;
+using Microsoft.EntityFrameworkCore;
 
 namespace TMS.TicketManagementSystem
 {
@@ -26,6 +28,8 @@ namespace TMS.TicketManagementSystem
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<TmsContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("TMSContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
