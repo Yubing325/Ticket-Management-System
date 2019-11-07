7cb1a2346a13e83a2fe941a11d91f7662a8d3faf
Yubing325 <35515298+Yubing325@users.noreply.github.com>
2019-11-07 11:19:11 -0600

    Boilerplate codes for ticketingManager.cs

---
+++ TMS.BusinessLogic/TicketingManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.BusinessLogic
+{
+    public class TicketingManager
+    {
+        public TicketingManager()
+        {
+
+        }
+
+        //Add a ticket
+
+        //Retrieve All Tickets
+
+        //Retrieve a specific ticket by ticketId
+
+        //Update a specific ticket
+
+        //Soft delete a ticket
+    }
+}
